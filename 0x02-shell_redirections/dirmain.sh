20e976bf6c50933c62c2c2da6b0fba492d3ccc0e
Tigo-cmd <ja16162040@gmail.com>
2023-05-15 12:21:25 -0400

    updated

---
+++ 0x02-shell_redirections/dirmain.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# automates directory creation.
+echo "dir >"
+read dir
+mkdir $dir
+cp dirmain.sh $dir
+cp dirgit.sh $dir
+cp filecreate.sh $dir
+cd $dir
+echo $dir > README.md
+bash dirgit.sh
+cd $dir
---
+++ 0x02-shell_redirections/filecreate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "file >"
+read file
+touch $file
+echo #!/bin/bash > $file
+echo "content >"
+read cnt
+echo $cnt >> $file
+chmod 764 $file
