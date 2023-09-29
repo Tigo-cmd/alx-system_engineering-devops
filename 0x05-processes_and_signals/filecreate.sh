87d07720b16578a3c660a767cd7a3d3ec0e7beec
Tigo-cmd <ja16162040@gmail.com>
2023-09-29 13:35:00 -0400

    List your processes

--- 0x05-processes_and_signals/filecreate.sh
+++ 0x05-processes_and_signals/filecreate.sh
@@ -2,9 +2,10 @@
 echo "file >"
 read file
 touch $file
-echo "#!/bin/bash" > $file
+echo "#!/usr/bin/env bash" > $file
 echo "cnt >"
 read cnt
 echo $cnt >> $file
 chmod 764 $file
 bash dirgit.sh
+gedit $file
