fcec4716ff947d2ab6090ca454eebbbbd758c85c
Tigo-cmd <ja16162040@gmail.com>
2023-10-03 13:57:40 -0400

    0. Simply matching School

--- 0x06-regular_expressions/filecreate.sh
+++ 0x06-regular_expressions/filecreate.sh
@@ -2,10 +2,10 @@
 echo "file >"
 read file
 touch $file
-echo "#!/usr/bin/env bash" > $file
+echo "#!/usr/bin/env ruby" > $file
 echo "cnt >"
 read cnt
-echo $cnt >> $file
+echo "puts ARGV[0].scan(/$cnt/).join" >> $file
 chmod 764 $file
 bash dirgit.sh
 gedit $file
