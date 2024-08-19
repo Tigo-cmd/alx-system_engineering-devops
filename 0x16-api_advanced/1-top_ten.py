bed3085d58712b3d193f47678eb90dc10d820d01
Tigo-cmd <ja16162040@gmail.com>
2024-08-19 15:02:46 +0100

    fixed: added logic to api url

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,21 +5,25 @@
 import json
 import requests
 
-
 def top_ten(subreddit):
     """
     a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
     :param subreddit: the subreddit channel
     :return: None and prints None
     """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     header = {
         "User-Agent": "Tigo: Pycharm2023.3.2"
     }
 
     response = requests.get(url, headers=header, allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    result = response.json().get("data").get("children")
-    [print(content.get("data").get("title")) for content in result]
+    try:
+        if response.status_code == 200:
+            result = response.json()
+            titles = result['data']['children']
+            for title in titles[:10]:
+                print(title["data"]['title'])
+        else:
+            print(None)
+    except requests.exceptions.RequestException:
+        print(None)
