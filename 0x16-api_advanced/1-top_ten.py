5421987da287997a20909be04f910542e87d0c4b
Tigo-cmd <ja16162040@gmail.com>
2024-08-19 14:59:10 +0100

    Fixed: request url

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,22 +12,14 @@ def top_ten(subreddit):
     :param subreddit: the subreddit channel
     :return: None and prints None
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
     header = {
         "User-Agent": "Tigo: Pycharm2023.3.2"
     }
 
-    # response = requests.get(url, headers=header, allow_redirects=False)
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
-
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
-    else:
-        print(None)
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    result = response.json().get("data").get("children")
+    [print(content.get("data").get("title")) for content in result]
