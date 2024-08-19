20e96b5acdc1329e92b4549a15f04a51a1d29004
Tigo-cmd <ja16162040@gmail.com>
2024-08-19 14:41:27 +0100

    satisfied puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,47 +1,6 @@
-# Ensure that the necessary directory exists
-file { '/etc/httpd/conf.d':
-  ensure => directory,
-}
-
-# Ensure that the necessary configuration file exists
-file { '/etc/httpd/conf.d/custom_config.conf':
-  ensure  => file,
-  content => "
-    # Example configuration
-    <VirtualHost *:80>
-        ServerAdmin webmaster@localhost
-        DocumentRoot /var/www/html
-
-        ErrorLog ${APACHE_LOG_DIR}/error.log
-        CustomLog ${APACHE_LOG_DIR}/access.log combined
-    </VirtualHost>
-  ',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  require => File['/etc/httpd/conf.d'],
-}
-
-# Ensure that Apache service is running
-service { 'httpd':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/httpd/conf.d/custom_config.conf'],
-}
-
-# Alternatively, if the issue was a permission problem
-file { '/var/www/html':
-  ensure  => directory,
-  owner   => 'apache',
-  group   => 'apache',
-  mode    => '0755',
-  recurse => true,
-  require => Package['httpd'],
-}
-
-# If SELinux is causing the issue (common in CentOS/RHEL)
-exec { 'restorecon apache content':
-  command => '/sbin/restorecon -Rv /var/www/html',
-  onlyif  => 'getenforce | grep -iq enforcing',
-  require => File['/var/www/html'],
+#script to fix a broken line in a file on a server
+#replace line containing "phpp" with "php"
+exec{ 'fix-wordpress':
+    command => "sudo sed -i \'s/.phpp/.php/g\' /var/www/html/wp-settings.php",
+    path    => ['/bin','/usr/bin']
 }
