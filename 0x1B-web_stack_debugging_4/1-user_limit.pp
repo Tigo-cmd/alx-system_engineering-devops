89b2d0deb366b05ccfe78eadf430f1325bd389ba
EmmanuelTigo <ja16162040@gmail.com>
2024-08-22 12:20:30 +0100

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,6 @@
 # with the holberton user and open a file without any error message.
 
 exec { 'fix_user_limit'
-    command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
-    path => '/usr/local/bin/:/bin/'
-}
-
-# Increase file limit for holberton ser.
-exec { 'increase_file':
-  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+    command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+    path => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
